ed593ec15751851c22aad47d3535d5d6f218c201
EdenHuang2056 <73862617+EdenHuang2056@users.noreply.github.com>
2021-03-09 11:32:38 +0800

    Add files via upload

---
+++ Assignment_3/Data.py
@@ -0,0 +1,51 @@
+
+import urllib.request as request
+import json
+
+def main():
+    src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
+    with request.urlopen(src) as response:
+        data = json.load(response)
+
+    # print(data)
+
+    big_list = data["result"]["results"]
+    small_list = []
+
+    with open("data.txt", "w", encoding="utf-8") as file:
+
+        for landmark in big_list:
+            title = file.write(landmark["stitle"]+",")
+            longitude = file.write(landmark["longitude"]+",")
+            latitude = file.write(landmark["latitude"]+",")
+            line = file.write(landmark["file"] +"\n")
+
+            # line = str(line)
+            # web = line.split(".")
+
+    # title=data["result"]["results"][1]["stitle"]
+    #
+    # longitude=data["result"]["results"][3]["longitude"]
+    #
+    # web=data["result"]["results"][14]["file"]
+    #
+    # latitude=data["result"]["results"][16]["latitude"]
+    #
+    # print(big_list)
+    # print(title)
+    # print(longitude)
+    # print(web)
+    # print(latitude)
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+    
